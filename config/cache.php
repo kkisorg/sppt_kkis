105a1787ab21a2fa14149a9416029b5bfdb3b97a
Martinus Alexander <martinus@Martinuss-MacBook-Pro.local>
2019-04-17 21:44:29 +0800

    Fix bug: Wrong environment variable

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'file'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
