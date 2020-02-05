2f9c9bc701390d64e78e2d37f99e6c33646fa20f
Martinus Alexander <martinus.alexander.looho@gmail.com>
2020-02-05 13:16:35 +0800

    Update form size

--- resources/views/monthlyofflinedistributionschedule/create.blade.php
+++ resources/views/monthlyofflinedistributionschedule/create.blade.php
@@ -26,7 +26,7 @@
 @section('content')
     @include('layout.message')
     <div class="row">
-        <div class="col xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+        <div class="col xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3><b>Form Jadwal Distribusi Bulanan Baru</b></h3>
